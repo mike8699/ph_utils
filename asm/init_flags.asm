9cb3888a0e7b18251353658f51eaa0e5ecaf243f
mikeoss <mikebuntu68@gmail.com>
2021-10-16 16:18:27 -0400

    set flags to 100% complete

--- asm/init_flags.asm
+++ asm/init_flags.asm
@@ -1,7 +1,7 @@
 ; This code will be executed immediately after a save file is loaded.
 ; Any flags that need to be set in memory can be set here.
 
-push r0, r3, r4 ; preserve original register values
+push r0, r2, r3, r4 ; preserve original register values
 
 ; Set r0 to the offset where flags start. In the vanilla rom, this is 0x21b553c.
 ; This is likely heap-allocated though, so the offset may change as we make modifications to the rom.
@@ -9,40 +9,55 @@ push r0, r3, r4 ; preserve original register values
 ; though it is offset by 0x30 so we must subtract that first.
 sub r4, lr, 0x30
 
-; Set bridge flag
-ldr r0, =0x20000
+ldr r0, =0xF77F7FEE
 str r0, [r4]
 
-// These don't work yet
-// ; Disable intro CG
-// ldr r0, =0x80000000
-// mov r3, 0x05
-// str r0, [r4, r3, lsl 0x2]
-// ; 
-// ; Disable Tetra "Help Me" CG
-// mov r0, 0x1
-// mov r3, 0x6
-// str r0, [r4, r3, lsl 0x2]
-// ; 
-// ; Disable Meeting Ciela CG
-// mov r0, 0x2
-// mov r3, 0xb
-// str r0, [r4, r3, lsl 0x2]
+ldr r0, =0xFFFFF8FF
+mov r3, 0x4
+str r0, [r4, r3]
 
-// ; Set initial Oshus talk flag
-// mov r0, 0x2 ;0x3
-// mov r3, 0x6
-// str r0, [r4, r3, lsl 0x2]
+ldr r0, =0xFD1FFFFF
+mov r3, 0x8
+str r0, [r4, r3]
 
-// ; Set earthquake Ciela dialog flag
-// ;ldr r0, =0x40000000
-// mov r0, 0x1
-// mov r3, 0xa; 0x7
-// str r0, [r4, r3, lsl 0x2]
+ldr r0, =0xFEFE3FDF
+mov r3, 0xC
+str r0, [r4, r3]
 
-// ; Set ChuChu first encounter flag
-// ldr r0, =0xC0000000
-// mov r3, 0x7
-// str r0, [r4, r3, lsl 0x2]
+ldr r0, =0x2FFF7FFD
+mov r3, 0x10
+str r0, [r4, r3]
 
-pop r0, r3, r4 ; restore original register values
+ldr r0, =0x7FFFFF03
+mov r3, 0x14
+str r0, [r4, r3]
+
+ldr r0, =0xFFF8C3FE
+mov r3, 0x18
+str r0, [r4, r3]
+
+ldr r0, =0xFFF5FEF5
+mov r3, 0x1C
+str r0, [r4, r3]
+
+ldr r0, =0xFFEFF86E
+mov r3, 0x20
+str r0, [r4, r3]
+
+ldr r0, =0xFFF0003D
+mov r3, 0x24
+str r0, [r4, r3]
+
+ldr r0, =0xFF0EFFFF
+mov r3, 0x28
+str r0, [r4, r3]
+
+ldr r0, =0x9FFFFFFD
+mov r3, 0x2C
+str r0, [r4, r3]
+
+ldr r0, =0x00000037
+mov r3, 0x30
+str r0, [r4, r3]
+
+pop r0, r2, r3, r4 ; restore original register values
