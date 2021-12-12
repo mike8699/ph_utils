79b64455f5bd6554ffb3fc79fbeda174800fcf11
mikeoss <mikebuntu68@gmail.com>
2021-12-12 14:57:30 -0500

    Add `setup.py` file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ph-utils',
+    version='0.0.1',
+    description='',
+    long_description='',
+    long_description_content_type='',
+    license='',
+    author='',
+    author_email='',
+    keywords='',
+    classifiers=[],
+    python_requires='>=3.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'ndspy',
+    ],
+    extras_require={
+        'test': [
+            'py-desmume',
+            'tox',
+        ]
+    },
+)
