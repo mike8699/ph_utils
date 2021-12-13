b7c62b3b482d91b156a0cc66d609ce823084cc32
mikeoss <mikebuntu68@gmail.com>
2021-12-12 23:41:52 -0500

    Fix args

--- test/test_boot.py
+++ test/test_boot.py
@@ -4,4 +4,4 @@ from test.conftest import DesmumeEmulator
 
 def test_boot_new_game(emulator_at_file_select: DesmumeEmulator):
     """Test bootup from title screen, name entry, and intro CG."""
-    emulator_at_file_select.touch_input(0, 130, 70)
+    emulator_at_file_select.touch_input(0, (130, 70))
