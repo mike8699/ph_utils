4251e40de3357f1fabe8d94c8835c02919a1549a
mikeoss <mikebuntu68@gmail.com>
2021-12-12 23:19:30 -0500

    Add basic test and enable pytest env in tox

---
+++ test/test_boot.py
@@ -0,0 +1,7 @@
+import pytest
+
+from test.conftest import DesmumeEmulator
+
+def test_boot_new_game(emulator_at_file_select: DesmumeEmulator):
+    """Test bootup from title screen, name entry, and intro CG."""
+    emulator_at_file_select.touch_input(0, 130, 70)
